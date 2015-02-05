665b2b98790a96894997b3c77e62786b84ad60c9
jtran90 <tran9960@gmail.com>
2015-02-05 10:32:56 +0100

    Create Client2.py

---
+++ Lab3/Client2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from socket import *
+serverName = "localhost"
+serverPort = 12346
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input("Input lowercase sentence:")
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+clientSocket.close()
