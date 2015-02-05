17142a6945ca1ad2e55de7a8d72a1f5e5e16153d
jtran90 <tran9960@gmail.com>
2015-02-05 10:33:55 +0100

    Create Server.py

---
+++ Lab3/Server.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+from socket import *
+serverPort = 12346
+serverhostIP = 'localhost'
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print 'The server is ready to receive'
+while 1:
+	message, clientAddress = serverSocket.recvfrom(2048)
+	modifiedMessage = message.upper()
+	serverSocket.sendto(modifiedMessage, clientAddress)
