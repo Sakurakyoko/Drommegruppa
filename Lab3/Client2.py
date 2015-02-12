70ec62a93a2a8a8212562f757e3d8b762ebed7db
afrosid <sidneyca@live.com>
2015-02-12 11:50:47 +0100

    Update Client2.py

--- Lab3/Client2.py
+++ Lab3/Client2.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from socket import *
+unitest = raw_input("Input for binary representation:")
 serverName = "localhost"
-serverPort = 12346
+serverPort = 12009
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = raw_input("Input lowercase sentence:")
-clientSocket.sendto(message,(serverName, serverPort))
+clientSocket.sendto(message, (serverName, serverPort))
+clientSocket.sendto(unitest, (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print modifiedMessage
+modifiedUni, serverAdress = clientSocket.recvfrom(2048)
+print "Binary representation of " + unitest + " = " + modifiedUni
+print "Lower case " + message + " = " + modifiedMessage
 clientSocket.close()
