1ff41a208136b17baa550ae1502b13fe6966343e
afrosid <sidneyca@live.com>
2015-02-12 11:51:07 +0100

    Update Server.py

--- Lab3/Server.py
+++ Lab3/Server.py
@@ -1,11 +1,16 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 from socket import *
-serverPort = 12346
-serverhostIP = 'localhost'
+def unicodeBin(character):
+    return '{0:08b}'.format(ord(character.decode('utf8')))
+serverPort = 12009
+# serverHostIp = "localhost"
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print 'The server is ready to receive'
+serverSocket.bind(("", serverPort))
+print "The server is ready to receive"
 while 1:
-	message, clientAddress = serverSocket.recvfrom(2048)
-	modifiedMessage = message.decode('utf-8').upper().encode('utf-8')
-	serverSocket.sendto(modifiedMessage, clientAddress)
+    message, clientAddress = serverSocket.recvfrom(2048)
+    unitest, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.decode("utf-8").upper().encode("utf-8")
+    modifiedUni = unicodeBin(unitest)
+    serverSocket.sendto(modifiedMessage, clientAddress)
+    serverSocket.sendto(modifiedUni, clientAddress)
